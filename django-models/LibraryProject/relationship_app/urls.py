d3d8ed9c67864270065e56cfb18821dd02ceadb8
David Oluyele <oluyeledavid@gmail.com>
2024-11-17 23:36:33 +0100

    Update templates directory, views.py and urls.py script

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,4 +5,8 @@ from .views import list_books
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # URL for function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # URL for class-based view
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -17,3 +17,36 @@ class LibraryDetailView(DetailView):
     def get_object(self):
         # Fetch the library by ID from the URL pattern
         return get_object_or_404(Library, pk=self.kwargs['pk'])
+
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")  # Update with the correct URL for redirection
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")  # Update with the correct URL for redirection
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, "logout.html")
