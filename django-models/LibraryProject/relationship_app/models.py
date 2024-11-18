4ed111df1f03d19512307c83551fce1162e6de08
David Oluyele <oluyeledavid@gmail.com>
2024-11-18 10:43:12 +0100

    Update models.py script

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+# Model representing an author
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+# Model representing a book
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -14,6 +18,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+# Model representing a library
 class Library(models.Model):
     name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
@@ -21,6 +26,7 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+# Model representing a librarian
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
@@ -28,14 +34,14 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
-# Define choices for roles
+# Choices for user roles
 ROLE_CHOICES = [
     ('Admin', 'Admin'),
     ('Librarian', 'Librarian'),
     ('Member', 'Member'),
 ]
 
-# Create a UserProfile model to link to the User model
+# UserProfile model to extend Django's built-in User model with a role
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
@@ -43,15 +49,13 @@ class UserProfile(models.Model):
     def __str__(self):
         return f'{self.user.username} - {self.role}'
 
-# Automatically create a UserProfile when a new User is created
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
+# Signal to automatically create a UserProfile when a new User is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
+# Signal to save the UserProfile when the User is saved
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
