d3334422fa3ff087a890a8dcc6c1343e829f0ae3
David Oluyele <oluyeledavid@gmail.com>
2024-11-29 13:29:22 +0100

    Update views.py and urls.py scripts

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,10 +2,17 @@
 
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
-from .models import Article
+from .models import Article, Book
 
 @permission_required('app_name.can_edit', raise_exception=True)
 def edit_article(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     # logic to edit the article
     return render(request, 'edit_article.html', {'article': article})
+
+def book_list(request):
+    # Fetch all books from the database
+    books = Book.objects.all()
+    # Render the book list to a template
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
