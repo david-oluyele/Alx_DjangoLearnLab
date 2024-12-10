a0bf16d1992bed412245ad1f8dd43688e88e8625
David Oluyele <oluyeledavid@gmail.com>
2024-12-10 02:09:53 +0100

    Implement Authentication and Permissions in DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -13,3 +14,5 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):  # Handles all CRUD operations
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access
+
