7e537f6a0e5b2e9e23c87f8ef27ea5d517432423
David Oluyele <oluyeledavid@gmail.com>
2024-12-15 23:04:46 +0100

    Update settings.py scripts

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',  # Your database name
+        'USER': 'root',  # Your MySQL username
+        'PASSWORD': '@tycoonTHEdeveloper0607',  # Your MySQL password
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
