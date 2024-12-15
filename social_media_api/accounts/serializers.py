2b215a2ef3ceb3437ab7198974f7fb49436fd0f2
David Oluyele <oluyeledavid@gmail.com>
2024-12-15 21:53:18 +0100

    Update serializers.py scripts

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,53 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+# Import your CustomUser model
 from .models import CustomUser
 
+# Get the custom user model
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        Token.objects.create(user=user)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+            if not user.check_password(password):
+                raise serializers.ValidationError("Invalid password.")
+        except User.DoesNotExist:
+            raise serializers.ValidationError("Invalid username.")
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return {'token': token.key}
+
+
+# Serializer for general user details
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
