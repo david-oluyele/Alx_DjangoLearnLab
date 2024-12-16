39a6fa552a19b9b7afca8f93b59ff1b14856672a
David Oluyele <oluyeledavid@gmail.com>
2024-12-16 02:27:23 +0100

    Update forms.py script and templates directory

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Post, Comment
 from taggit.forms import TagWidget
+from django.forms import widgets
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -21,9 +22,20 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content', 'tags']
 
+   # Example of using a standard widget from django.forms
+    title = forms.CharField(
+        widget=widgets.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title here'}),
+        required=True
+    )
+    content = forms.CharField(
+        widget=widgets.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter post content here'}),
+        required=True
+    )
+    
+    # Example of using the TagWidget from django-taggit
     tags = forms.CharField(
-        widget=TagWidget(),  # Use TagWidget to handle the tag input
-        required=False
+        widget=TagWidget(),  # TagWidget from django-taggit to handle input
+        required=False  # Tags are optional
     )
 
 class CommentForm(forms.ModelForm):
